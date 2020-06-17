bb4e1ab28783ad17f350b1d28596e7ef5be21596
FWdarling <1853204@tongji.edu.cn>
2020-06-18 00:15:58 +0800

    finish LRU and display the result

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 
 var blockNum = 4, instructionNum = 320;
-var memory = Array(); //�ڴ��
-var missingPages = 0; //ȱҳ����
-var currentIns;//��ǰָ��
-var runTimesCnt = 0;//���д���
-var bIsMissing;//��ǰָ���Ƿ�ȱҳ
-var memoryBlock;//��ǰָ�������ڴ��
+var memory = Array(); 
+var missingPages = 0; 
+var currentIns;
+var runTimesCnt = 0;
+var bIsMissing;
+var memoryBlock;
 var fifoQueue = Array();
+var lruQueue = Array();
 
 
 var currentInsSpan = document.getElementById("current_instruction");
@@ -26,6 +27,7 @@ function getRandInt(min, max){
 function initialize(){
     memory = [];
     fifoQueue = [];
+    lruQueue = [];
     missingPages = 0;
     currentIns = getRandInt(0, 319);
     runTimesCnt = 0;
@@ -41,6 +43,9 @@ function initialize(){
     document.getElementById("algorithm").disabled = false;
     document.getElementById("execution").disabled = false;
     document.getElementById("run-btn").disabled = false;
+
+    var table = document.getElementById("tbody");
+    table.lastChild.remove();
 }
 
 function updatePage(){
@@ -61,15 +66,11 @@ function findPage(page, mem){
 function FIFO(ins){
     var page = Math.floor(ins / 10);
     var findRes = findPage(page, memory);
+    var tag = false;
     //命中
     if(findRes >= 0){
         memoryBlock = findRes + 1;
         bIsMissing = false;
-        var res = findPage(page, fifoQueue);
-        for(var i = res; i < fifoQueue.length - 1; i++){
-            fifoQueue[i] = fifoQueue[i + 1];
-        }
-        fifoQueue[fifoQueue.length - 1] = page;
     }
     
     //不命中
@@ -85,6 +86,7 @@ function FIFO(ins){
         }
         //替换
         else{
+            tag = true;
             var pageReplaced = fifoQueue[0];
             var memReplaced = findPage(pageReplaced, memory);
             memory[memReplaced] = page;
@@ -95,11 +97,77 @@ function FIFO(ins){
             fifoQueue[3] = page;
         }
     }
+    var newRow = document.getElementById("tbody").insertRow();
+    newRow.insertCell(0).innerHTML = ins;
+    newRow.insertCell(1).innerHTML = memory[0] == undefined ? "Empty" : memory[0];
+    newRow.insertCell(2).innerHTML = memory[1] == undefined ? "Empty" : memory[1];
+    newRow.insertCell(3).innerHTML = memory[2] == undefined ? "Empty" : memory[2];
+    newRow.insertCell(4).innerHTML = memory[3] == undefined ? "Empty" : memory[3];
+    if(findRes >= 0){
+        newRow.insertCell(5).innerHTML = "命中: " + ins + "在第" + (findRes + 1) + "块内存中";
+    }
+    else if(!tag){
+        newRow.insertCell(5).innerHTML = "缺页调入: 将第" + page + "页调入第" + memoryBlock + "块内存";
+    }
+    else{
+        newRow.insertCell(5).innerHTML = "缺页替换: 将第" + memoryBlock + "块内存替换为第" + page + "页";
+    }
 }
 
 function LRU(ins){
-
+    var page = Math.floor(ins / 10);
+    var findRes = findPage(page, memory);
+    var tag = false;
+    //命中
+    if(findRes >= 0){
+        memoryBlock = findRes + 1;
+        bIsMissing = false;
+        lruQueue[findRes] = runTimesCnt;
+    }
+    
+    //不命中
+    else{
+        bIsMissing = true;
+        missingPages++;
+        var length = memory.length;
+        //调入
+        if(length < 4){
+            memory[length] = page;
+            memoryBlock = length + 1;
+            lruQueue[length] = runTimesCnt;
+        }
+        //替换
+        else{
+            tag = true;
+           var leastInsIndex, leastIns = 100000;
+           for(var i = 0; i < 4; i++){
+                if(lruQueue[i] < leastIns){
+                    leastIns = lruQueue[i];
+                    leastInsIndex = i;
+                }
+           }
+           memory[leastInsIndex] = page;
+           memoryBlock = leastInsIndex + 1;
+           lruQueue[leastInsIndex] = runTimesCnt;
+        }
+    }
+    var newRow = document.getElementById("tbody").insertRow();
+    newRow.insertCell(0).innerHTML = ins;
+    newRow.insertCell(1).innerHTML = memory[0] == undefined ? "Empty" : memory[0];
+    newRow.insertCell(2).innerHTML = memory[1] == undefined ? "Empty" : memory[1];
+    newRow.insertCell(3).innerHTML = memory[2] == undefined ? "Empty" : memory[2];
+    newRow.insertCell(4).innerHTML = memory[3] == undefined ? "Empty" : memory[3];
+    if(findRes >= 0){
+        newRow.insertCell(5).innerHTML = "命中: " + ins + "在第" + (findRes + 1) + "块内存中";
+    }
+    else if(!tag){
+        newRow.insertCell(5).innerHTML = "缺页调入: 将第" + page + "页调入第" + memoryBlock + "块内存";
+    }
+    else{
+        newRow.insertCell(5).innerHTML = "缺页替换: 将第" + memoryBlock + "块内存替换为第" + page + "页";
+    }
 }
+
 
 function run(algo){
     runTimesCnt++;
