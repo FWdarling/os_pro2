9188695d08307554171d804e0be55d6c0f7bfc18
FWdarling <1853204@tongji.edu.cn>
2020-06-17 18:16:03 +0800

    finish FIFO algorirthm

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 
 var blockNum = 4, instructionNum = 320;
-var memory = Array(blockNum); //�ڴ��
+var memory = Array(); //�ڴ��
 var missingPages = 0; //ȱҳ����
 var currentIns;//��ǰָ��
 var runTimesCnt = 0;//���д���
 var bIsMissing;//��ǰָ���Ƿ�ȱҳ
 var memoryBlock;//��ǰָ�������ڴ��
+var fifoQueue = Array();
 
 
 var currentInsSpan = document.getElementById("current_instruction");
@@ -24,6 +25,7 @@ function getRandInt(min, max){
 
 function initialize(){
     memory = [];
+    fifoQueue = [];
     missingPages = 0;
     currentIns = getRandInt(0, 319);
     runTimesCnt = 0;
@@ -43,14 +45,56 @@ function initialize(){
 
 function updatePage(){
     currentInsSpan.textContent = currentIns;
-    physicalMemSpan.textContent = ("第" + Math.floor(currentIns / 10) + "页的首地址+" + (currentIns % 10));
+    physicalMemSpan.textContent = ("第" + (Math.floor(currentIns / 10) + 1) + "页的首地址+" + (currentIns % 10));
     memoryBlockSpan.textContent = memoryBlock;
     isMissingSpan.textContent = (bIsMissing ? "是" : "否");
     missingPageRateSpan.textContent = Math.round(missingPages / runTimesCnt * 100) + "%";
 }
 
-function FIFO(ins){
+function findPage(page, mem){
+    for(var i = 0; i < mem.length; i++){
+        if(mem[i] === page)  return i;
+    }
+    return -1;
+}
 
+function FIFO(ins){
+    var page = Math.floor(ins / 10);
+    var findRes = findPage(page, memory);
+    //命中
+    if(findRes >= 0){
+        memoryBlock = findRes + 1;
+        bIsMissing = false;
+        var res = findPage(page, fifoQueue);
+        for(var i = res; i < fifoQueue.length - 1; i++){
+            fifoQueue[i] = fifoQueue[i + 1];
+        }
+        fifoQueue[fifoQueue.length - 1] = page;
+    }
+    
+    //不命中
+    else{
+        bIsMissing = true;
+        missingPages++;
+        var length = memory.length;
+        //调入
+        if(length < 4){
+            memory[length] = page;
+            memoryBlock = length + 1;
+            fifoQueue[length] = page;
+        }
+        //替换
+        else{
+            var pageReplaced = fifoQueue[0];
+            var memReplaced = findPage(pageReplaced, memory);
+            memory[memReplaced] = page;
+            memoryBlock = memReplaced + 1;
+            for(var i = 0; i < 3; i++){
+                fifoQueue[i] = fifoQueue[i + 1];
+            }
+            fifoQueue[3] = page;
+        }
+    }
 }
 
 function LRU(ins){
